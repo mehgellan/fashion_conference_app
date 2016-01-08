683fd1fe637a03efd94297de68965f5e2bb280ee
nathanallen <thenathanator@gmail.com>
2016-01-07 17:00:55 -0800

    custom speaker instance methods

--- conference_app/app/models/speaker.rb
+++ conference_app/app/models/speaker.rb
@@ -1,2 +1,12 @@
 class Speaker < ActiveRecord::Base
+
+  def speechify(blabber_duration=1)
+    FFaker::HipsterIpsum.paragraph(blabber_duration)
+  end
+
+  def to_s
+    # alligator ascii
+    "            .-._   _ _ _ _ _ _ _ _\n .-''-.__.-'00  '-' ' ' ' ' ' ' ' '-.\n'.___ '    .   .--_'-' '-' '-' _'-' '._\n V: V 'vv-'   '_   '.       .'  _..' '.'.\n   '=.____.=_.--'   :_.__.__:_   '.   : :\n           (((____.-'        '-.  /   : :\n snd                         (((-'\ .' /\n                           _____..'  .'\n                          '-._____.-'"
+  end
+
 end
