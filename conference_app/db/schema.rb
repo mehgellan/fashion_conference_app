bc678205e0b592b8cfc9564008850dd30f725697
nathanallen <thenathanator@gmail.com>
2016-01-07 17:00:29 -0800

    migrate speaker

--- conference_app/db/schema.rb
+++ conference_app/db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160107224819) do
+ActiveRecord::Schema.define(version: 20160108001349) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "speakers", force: :cascade do |t|
+    t.string   "first"
+    t.string   "last"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "talks", force: :cascade do |t|
     t.string   "topic"
