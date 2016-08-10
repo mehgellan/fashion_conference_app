60dd30d5e579216072ff7a48a2dd4a89287ac63d
mehgellan <baumegg@gmail.com>
2016-08-10 13:33:31 -0700

    seed db with ffaker

--- conference_app/app/models/speaker.rb
+++ conference_app/app/models/speaker.rb
@@ -1,3 +1,6 @@
 class Speaker < ActiveRecord::Base
   validates :email, presence: true
+  def speechify(blabber_duration=1) # default value
+    FFaker::HipsterIpsum.sentence(blabber_duration)
+  end
 end
